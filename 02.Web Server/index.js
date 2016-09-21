0f287ca5ed2c3f33e0dfd0d39cdf79395cd1de38
Stanislav Kozlovski <familyguyuser192@windowslive.com>
2016-09-21 16:03:17 +0300

    Added problem 3 - Create HTML with a form Users should be able to fill name and URL of an image and send the data to the server. The request should be POST. The server should save the name and the URL in a memory array. If the name or the URL are null or empty, the server should return a friendly error message.

--- 02.Web Server/index.js
+++ 02.Web Server/index.js
@@ -1,13 +1,50 @@
 let http = require('http')
 let handlers = require('./handlers/index')
+let qs = require('querystring')
 let port = 1337
+let images = {}
+let savedImages = false
 
 http.createServer((req, res) => {
+  if (req.method === 'POST') {
+    var body = ''
+
+    req.on('data', function (data) {
+      body += data
+
+      // Too much POST data, kill the connection!
+      // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+      if (body.length > 1e6) {
+        req.connection.destroy()
+      }
+    })
+
+    savedImages = req.on('end', function () {
+      let post = qs.parse(body)
+      let imageName = post['imagename']
+      let imageUrl = post['imageurl']
+
+      if (imageName.length === 0 || imageUrl.length === 0) {
+        // ERROR
+        res.writeHead(404)
+        res.write('The name and URL of the image cannot be empty.')
+        res.end()
+        return false
+      }
+
+      images[imageName] = imageUrl
+      return true
+    })
+  }
+
+  if (!savedImages) {
   for (let handler of handlers) {
     let next = handler(req, res)
     if (!next) {
       break
     }
   }
+  }
+
 })
 .listen(port)
