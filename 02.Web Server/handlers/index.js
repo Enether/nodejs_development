931eba053cd7f2a546e13efcf1619d0aa499ebee
Stanislav Kozlovski <familyguyuser192@windowslive.com>
2016-09-21 17:44:04 +0300

    Added a link and a handler to the details.html/js page.

--- 02.Web Server/handlers/index.js
+++ 02.Web Server/handlers/index.js
@@ -3,10 +3,12 @@ Module returns the lsit of all the handlers for us to iterate through
 */
 let favicon = require('./favicon')
 let homePage = require('./home-page')
+let detailsPage = require('./details-page')
 let staticFiles = require('./static-files')
 
 module.exports = [
   favicon,
   homePage,
+  detailsPage,
   staticFiles
 ]
