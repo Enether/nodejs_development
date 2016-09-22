49804f7bd0beee65ab73582ba364222bdb7ce003
Stanislav Kozlovski <familyguyuser192@windowslive.com>
2016-09-22 21:31:10 +0300

    Moved the forms to submit an image to a separate html file and added handlers for that.

--- 02.Web Server/handlers/index.js
+++ 02.Web Server/handlers/index.js
@@ -3,12 +3,14 @@ Module returns the lsit of all the handlers for us to iterate through
 */
 let favicon = require('./favicon')
 let homePage = require('./home-page')
+let addImagePage = require('./add-image')
 let detailsPage = require('./details-page')
 let staticFiles = require('./static-files')
 
 module.exports = [
   favicon,
   homePage,
+  addImagePage,
   detailsPage,
   staticFiles
 ]
