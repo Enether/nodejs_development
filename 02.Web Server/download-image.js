64f267242f65af8ec98ae05eeacb767c5a50896f
Stanislav Kozlovski <familyguyuser192@windowslive.com>
2016-09-22 21:16:30 +0300

    Changed the path to the downloaded images to /content/images. Also added functionality to create the folder if it doesn't exist.

--- 02.Web Server/download-image.js
+++ 02.Web Server/download-image.js
@@ -1,19 +1,28 @@
 // this modules downloads an image the user has entered
 let http = require('http')
 let fs = require('fs')
+let imageDir = './content/images/'
+
 // TODO: https too
 function downloadImage (imageUrl, imageName, callback) {
   let file = fs.createWriteStream(imageName)
-  let request = http.get(imageUrl, function (response) {
+
+  // request for the image
+  http.get(imageUrl, function (response) {
     response.pipe(file)
   })
   callback()
 }
 
+// create the image folder if it doesnt exist
+if (!fs.exists(imageDir)) {
+  fs.mkdirSync(imageDir)
+}
+
 
 module.exports = (imageurl, imagename) => {
   // downloads the image
-  downloadImage(imageurl, './images/' + imagename + '.jpg', function () {
+  downloadImage(imageurl, './content/images/' + imagename + '.jpg', function () {
     console.log('Done downloading image ' + imagename)
   })
 }
