20e313c92cc6c8ffc2d231e28d770465898aa925
Stanislav Kozlovski <familyguyuser192@windowslive.com>
2016-09-30 18:04:07 +0300

    Refactored and tweaked the generate-unique-image-url.js file so as to not overwrite the file with a new blank .json if the file already existed. This happened because of the asynchronous fs.exists function. Replaced it with it's synchronous equivalent.

--- 03.Web Server with File Upload/generate-unique-image-url.js
+++ 03.Web Server with File Upload/generate-unique-image-url.js
@@ -1,11 +1,13 @@
+/* this module handles the generation of unique private image URLs and the storage of that information information
+a JSON file, called image-urls.json */
+
 let shortid = require('shortid')
 let fs = require('fs')
+let imageUrlsPath = './image-urls.json'
 
-if (!fs.exists('./image-urls.json')) {
-  fs.writeFile('./image-urls.json', '{}')
+if (!fs.existsSync(imageUrlsPath)) {
+  fs.writeFile(imageUrlsPath, '{}')
 }
-
-
 
 function updateJSONFile (imagePath, imageName) {
   // generate unique image url
@@ -16,7 +18,7 @@ function updateJSONFile (imagePath, imageName) {
 
   // update the json
   parsedJSON[imageUrl] = {'path': imagePath, 'name': imageName}
-  console.log(imageUrl)
+  console.log('The hidden URL of ' + imageName + ' is: ' + imageUrl)
   // save the file
   fs.writeFile('./image-urls.json', JSON.stringify(parsedJSON))
 }
