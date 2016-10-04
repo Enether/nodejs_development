e92d9196918b0b2fe991b7772fa78b75a29ce134
Stanislav Kozlovski <familyguyuser192@windowslive.com>
2016-10-04 18:54:51 +0300

    Added 4.2 Route GET /create The server should be able to return a simple form for creating a TODO entry. Each TODO entry should have Title, Description and State. State can be “Pending” and “Done”. All TODO entries are initially in “Pending” state. The form should have two inputs – one for the title and one for the description.

---
+++ 04.Workshop - TODO list web server/handlers/create-task.js
@@ -0,0 +1,22 @@
+/* this module handles GET requests to the homepage, effectively displaying our index.html */
+let url = require('url')
+let fs = require('fs')
+let indexPagePath = './add-todo-task.html'
+
+module.exports = (req, res) => {
+  req.pathName = req.pathName || url.parse(req.url).pathname
+
+  if (req.pathName === '/create') {
+    fs.readFile(indexPagePath, (err, data) => {
+      if (err) console.log(err.message)
+      // display index.html
+      res.writeHead(200, {
+        'Content-Type': 'text/html'
+      })
+      res.write(data)
+      res.end()
+    })
+  } else {
+    return true
+  }
+}
--- 04.Workshop - TODO list web server/handlers/handlers.js
+++ 04.Workshop - TODO list web server/handlers/handlers.js
@@ -1,6 +1,7 @@
 /* this module returns an array of all our handers */
 let homePage = require('./home-page')
-
+let createTodoPage = require('./create-task')
 module.exports = [
-  homePage
+  homePage,
+  createTodoPage
 ]
