5aa8221c0ebc5e0093179e8cdd7996fe4326e3d9
Stanislav Kozlovski <familyguyuser192@windowslive.com>
2016-10-08 18:03:38 +0300

    Improved the dynamically-generated details-page.html file.
    The button now correctly changes between "DONE" and "Change to pending" in regards to the TODO task's current state
    :)
    The TODO's state is now correctly changed on button click
    Made the page load the initial state paragraph and button text based on the TODO's state, not the hard-coded text in the HTML

--- 04.Workshop - TODO list web server/handlers/GET/details-page/create-details-page.js
+++ 04.Workshop - TODO list web server/handlers/GET/details-page/create-details-page.js
@@ -8,20 +8,32 @@ let TODO_STATE_KEY = 'state'
 let TODO_INDEX_KEY = 'index'
 let TODO_COMMENTS_KEY = 'comments'
 
+let PENDING_PARAGRAPH_TEXT = 'PENDING...'
+let DONE_PARAGRAPH_TEXT = 'DONE'
+
+function getTodoState (todo) {
+  // gets the state of TODO task to be shown on the page
+  let stateParagraphText = ''
+  let todoState = todo[TODO_STATE_KEY]
+
+  if (todoState === 'pending') {
+    stateParagraphText = PENDING_PARAGRAPH_TEXT
+  } else {
+    stateParagraphText = DONE_PARAGRAPH_TEXT
+  }
+
+  return stateParagraphText
+}
+
 function createHTML (todo) {
-    // this function creates the html that displays information about a TODO
+  // this function creates the html that displays information about a TODO
   let body = ''
+  // TODO Title and Description
   body += '<h1>' + todo[TODO_TITLE_KEY] + '</h1>'
   body += '<p>' + todo[TODO_DESCRIPTION_KEY] + '</p>'
 
-  let stateParagraphText = ''
-  let todoState = todo[TODO_STATE_KEY]
-  if (todoState === 'pending') {
-    stateParagraphText = 'PENDING...'
-  } else {
-    stateParagraphText = 'DONE'
-  }
-  body += '<p id="stateParagraph">' + stateParagraphText + '</p>'
+  // the TODO's state. It is interchangeable
+  body += '<p id="stateParagraph"></p>'
 
   // add an image if there is one
   if (todo.imagePath) {
@@ -36,26 +48,33 @@ function createHTML (todo) {
   }
 
   body += '<form id="stateForm" method="POST" action="/" enctype="multipart/form-data">'
-  body += '<input type="hidden" name="state" value="pending">'
-  body += '<input type="hidden" name="todoIndex" value="' + todo[TODO_INDEX_KEY] + '">'
+  // add the TODO's state input, this is read later so that it can be converted to the opposite state once the user wants to change it
+  body += '<input type="hidden" name="state" id="state" value="' + todo[TODO_STATE_KEY] + '">'
+  // add the TODO's index
+  body += '<input type="hidden" name="todoIndex" id="todoIndex" value="' + todo[TODO_INDEX_KEY] + '">'
+  // the text area for the ability to add a comment to a TODO
   body += '<textarea rows="4" cols="50" name="comment">Enter comment here...</textarea>'
-  body += '<button type="submit" onclick="changeState();" id="changeStateButton">DONE</button>'
+  // The button that changes the TODO's state and a comment the user has given
+  body += '<button type="submit" onclick="changeState();" id="changeStateButton">Change to pending</button>'
+  // in-html javascript script to change the displayed state and button text when the state is changed.
   body += '<script>'
+  body += 'changeState();'  // call the function on HTML load in case the TODO's state has been previously changed
   body += '  function changeState(){'
   body += 'state = document.getElementById("stateParagraph").innerHTML;'
   body += 'if (state === "PENDING...") {'
-  body += 'document.getElementById("stateParagraph").innerHTML = "DONE!";'
-  body += 'document.getElementById("state").setAttribute("value","done");'
-  body += 'document.getElementById("changeStateButton").innerHTML = "Change to pending";'
+  body += 'document.getElementById("stateParagraph").innerHTML = "DONE!";'  // change state paragraph
+  body += 'document.getElementById("changeStateButton").innerHTML = "Change to pending";'  // change button text
+  body += 'document.getElementById("state").value = "done";'
   body += 'document.getElementById("stateForm").submit();'
   body += '} else {'
-  body += 'document.getElementById("stateParagraph").innerHTML = "PENDING...";'
-  body += 'document.getElementById("state").setAttribute("value","pending");'
-  body += 'document.getElementById("changeStateButton").innerHTML = "DONE";'
+  body += 'document.getElementById("stateParagraph").innerHTML = "PENDING...";'  // change state paragraph
+  body += 'document.getElementById("changeStateButton").innerHTML = "DONE";'  // change button text
+  body += 'document.getElementById("state").value = "pending";'  // change the state's value so that we can read it later in the post request
   body += 'document.getElementById("stateForm").submit();'
   body += '}'
   body += '}'
   body += '</script>'
+  // END OF in-html javascript script to change the displayed state and button text when the state is changed.
   body += '</form>'
 
   return '<!DOCTYPE html><html><header></header><body>' + body + '</body></html>'
